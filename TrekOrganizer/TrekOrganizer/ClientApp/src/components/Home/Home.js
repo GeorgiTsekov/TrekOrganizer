9801df2993f1ea54a4ade68b751b7fb32b8076e7
Georgi Tsekov <georgi_tsekov@abv.bg>
2021-04-02 17:31:17 +0300

    Initial TrekOrganizer with asp.net + react

---
+++ TrekOrganizer/TrekOrganizer/ClientApp/src/components/Home/Home.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+export class Home extends Component {
+    static displayName = Home.name;
+
+    render() {
+        return (
+            <div className="home d-md-flex flex-md-equal  my-md-3 pl-md-3">
+                <div className="col-md-7">
+                    <h2 className="featurette-heading">Here you will find the treks you've <span className="text-highlighted">always dreamed
+          about!</span></h2>
+                    <p className="lead">1. Join our family.</p>
+                    <p className="lead">2. Request the trek you've always wanted.</p>
+                    <p className="lead">3. Find people who like your idea.</p>
+                    <p className="lead">If you find enough supporters, we will organize everything for you!</p>
+                </div>
+                <div className="col-md-5">
+                    <img className="home-picture" src="https://i.pinimg.com/originals/b1/5e/2d/b15e2d7d7aad22ba98e7e4ff5c973515.jpg" />
+                </div>
+            </div>
+        );
+    }
+}
